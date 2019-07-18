301b77fc6fe131f289edd61c17b177762dd3ec65
silashuereca <silasgranthuereca@gmail.com>
2019-07-18 12:45:20 -0500

    Updates

---
+++ precache-manifest.c09fe6f61a4e4122c44f06ea81859972.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee90375d31afabbec06669d3421d1eee",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "729c9e1d679e48aa96ee",
+    "url": "/shopping-cart/static/css/main.27ad637f.chunk.css"
+  },
+  {
+    "revision": "0440f5c61b89d51833e3",
+    "url": "/shopping-cart/static/js/2.97294558.chunk.js"
+  },
+  {
+    "revision": "729c9e1d679e48aa96ee",
+    "url": "/shopping-cart/static/js/main.35722268.chunk.js"
+  },
+  {
+    "revision": "3675f54918f9d621420d",
+    "url": "/shopping-cart/static/js/runtime~main.60a85d2f.js"
+  }
+]);
