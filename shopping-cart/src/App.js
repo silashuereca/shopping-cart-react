310f96c9118ab30bbdc380df14b6ee4f1cc0d20f
silashuereca <silasgranthuereca@gmail.com>
2019-07-17 20:06:53 -0500

    Add button onClick to each product

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -9,6 +9,15 @@ import Cart from "./Components/Cart"
 class App extends Component {
   state = {
     cartPosition: -500,
+    products: [
+      {product: 'Shoes', price: 9.99, key: 1},
+      {product: 'Pants', price: 45.99, key: 2},
+      {product: 'Shirt', price: 25.99, key: 3},
+      {product: 'Hat', price: 45.50, key: 4},
+      {product: 'Socks', price: 13.70, key: 5},
+      {product: 'Boxers', price: 25.89, key: 6}
+    ],
+    cartProducts: [],
   }
 
   // hiding the cart panel or closing it
@@ -18,18 +27,34 @@ class App extends Component {
     })
   }
 
+// opening cart panel
   handleOpen = () => {
     return this.setState({
       cartPosition: 0,
     })
   }
+
+// Add product to cart
+handleAddProduct = (e) => {
+const value = e.target.value
+const newArray = value.split(' ');
+  
+  const newArray = [...this.state.cartProducts, {value}]
+
+  this.setState({
+    cartProducts: newArray
+  })
+  
+}
+  
   render () {
+    console.log(this.state.cartProducts)
     return (
       <div className="App">
       <CartBtn onOpen={this.handleOpen}/>
       <Cart position={this.state.cartPosition} onClose={this.handleClose}/>
       <Header />
-      <ProductList />
+      <ProductList onAddProduct={this.handleAddProduct} products={this.state.products}/>
     </div>
     )
   }
--- shopping-cart/src/Components/ProductList.js
+++ shopping-cart/src/Components/ProductList.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
 
-const ProductList = () => {
+const ProductList = (props) => {
     return (
         <div>
             <h1>Products</h1>
+            <div className="product-container">
+            {props.products.map(item => {
+                return (
+                    <div key={item.key} >
+                        <h3>{item.product}</h3> 
+                        <p>{item.price}</p>
+                        <button 
+                        onClick={props.onAddProduct} 
+                        value={item.product + ' ' + item.price}
+                        >Buy</button>
+                        </div>
+                )
+            })}
+            </div>
         </div>
     )
 }
